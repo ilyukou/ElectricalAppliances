41d5960c7c7162bedd48d69630f912b89311d174
Ilya Ilyukou <ilyailyukou@gmail.com>
2019-10-27 16:25:22 +0300

    add ElectricApplianceComparatorByPowerTest

---
+++ src/test/java/electricAppliance/model/comporators/ElectricApplianceComparatorByPowerTest.java
@@ -0,0 +1,44 @@
+package electricAppliance.model.comporators;
+
+import electricAppliance.model.ElectricAppliance;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.when;
+
+class ElectricApplianceComparatorByPowerTest {
+
+    private ElectricAppliance first = Mockito.mock(ElectricAppliance.class);
+    private ElectricAppliance second = Mockito.mock(ElectricAppliance.class);
+
+    private ElectricApplianceComparatorByPower comparatorByPower = new ElectricApplianceComparatorByPower();
+
+    @Test
+    void compare_whenFirstMoreThanSecond() {
+
+        when(first.getPower()).thenReturn(10);
+        when(second.getPower()).thenReturn(5);
+
+        int res = comparatorByPower.compare(first,second);
+        assertEquals(1,res);
+    }
+
+    @Test
+    void compare_whenSecondMoreThanFirst() {
+        when(first.getPower()).thenReturn(10);
+        when(second.getPower()).thenReturn(5);
+
+        int res = comparatorByPower.compare(second,first);
+        assertEquals(-1,res);
+    }
+
+    @Test
+    void compare_whenBothEqual() {
+        when(first.getPower()).thenReturn(10);
+        when(second.getPower()).thenReturn(10);
+
+        int res = comparatorByPower.compare(first,second);
+        assertEquals(0,res);
+    }
+}
